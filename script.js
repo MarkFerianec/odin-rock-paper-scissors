ccc692cbb6146ac9fcef3f71c1b75c19658867e9
Mark Ferianec <134004771+MarkFerianec@users.noreply.github.com>
2023-06-10 15:12:40 -0600

    Add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    let number = Math.floor(Math.random() * 3) + 1;
+    switch (number) {
+        case 1:
+            return 'Rock';
+        case 2:
+            return 'Paper';
+        case 3:
+            return 'Scissors';
+    }
+}
