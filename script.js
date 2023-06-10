67006fbbfa53e1fb7ecfaa5eb219f1e4d6dd5b8e
Mark Ferianec <134004771+MarkFerianec@users.noreply.github.com>
2023-06-10 16:31:39 -0600

    Add playRound function

--- script.js
+++ script.js
@@ -9,3 +9,36 @@ function getComputerChoice() {
             return 'Scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    let playerSelectionLowerCase = playerSelection.toLowerCase();
+    let computerSelectionLowerCase = computerSelection.toLowerCase();
+
+    if (playerSelectionLowerCase == 'rock' && computerSelectionLowerCase == 'scissors') {
+        return "You win! Rock beats Scissors";
+    }
+    else if (playerSelectionLowerCase == 'rock' && computerSelectionLowerCase == 'paper') {
+        return "You lose! Paper beats Rock";
+    }
+    else if (playerSelectionLowerCase == 'paper' && computerSelectionLowerCase == 'rock') {
+        return "You win! Paper beats Rock";
+    }
+    else if (playerSelectionLowerCase == 'paper' && computerSelectionLowerCase == 'scissors') {
+        return "You lose! Scissors beats Paper";
+    }
+    else if (playerSelectionLowerCase == 'scissors' && computerSelectionLowerCase == 'paper') {
+        return "You win! Scissors beats Paper";
+    }
+    else if (playerSelectionLowerCase == 'scissors' && computerSelectionLowerCase == 'rock') {
+        return "You lose! Rock beats Scissors";
+    }
+    else {
+        return "Tie";
+    }
+}
+
+const playerSelection = "rock";
+// console.log("Player selection is " + playerSelection);
+const computerSelection = getComputerChoice();
+// console.log("Computer selection is " + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
